d429bf624a6b3033e5dbfa13f545e7fa80df1242
Charlotte Rushen <charlotterushen@hotmail.com>
2019-11-27 15:00:02 +0000

    Added AOS

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueResource from 'vue-resource';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+Vue.use(VueResource);
+
+Vue.http.options.root = 'http://localhost:8000/wp-json/'; // change this to website url when going into production
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    AOS.init({
+      duration: 500,
+      easing: "ease-in-out",
+      once: true
+    });
+  }
 }).$mount('#app')
