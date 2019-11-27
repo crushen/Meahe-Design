6deef997602d24ce8771dd90774ccc7acb692a19
Charlotte Rushen <charlotterushen@hotmail.com>
2019-11-27 12:05:47 +0000

    Started to recreate nav

---
+++ src/components/nav/Nav.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav>
+
+
+    <div id="nav">
+      <router-link to="/">Projects</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+
+
+    <!-- <div>
+      <div class="nav-bar"></div>
+      <img :src="logo" alt="An orange circle with the Méahé Design logo inside" class="logo">
+
+      <div class="burger" @click="toggleNav">
+        <div class="burger-bar"></div>
+      </div>
+
+      <transition name="slide" enter-active-class="animated slideInDown faster" leave-active-class="animated slideOutUp faster">
+        <nav class="nav" v-if="navActive">
+          <router-link to="/" exact>Home</router-link>
+          <router-link to="/brands">Brands</router-link>
+          <router-link to="/side-projects">Side Projects</router-link>
+        </nav>
+      </transition>
+
+    </div> -->
+
+
+  </nav>
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
