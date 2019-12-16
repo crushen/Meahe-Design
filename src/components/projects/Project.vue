2aab1caf0554371c48903f5ccd65472ee9b79bdc
Charlotte Rushen <charlotterushen@hotmail.com>
2019-12-16 10:52:44 +0000

    Organic grid for projects desktop

--- src/components/projects/Project.vue
+++ src/components/projects/Project.vue
@@ -2,13 +2,13 @@
   <section id="project" class="nav-padding">
     <div v-for="proj in project" :key="proj.id">
       <info-button :project="project" />
-      <img  v-for="img in proj.acf.gallery"
-            :key="img.id"
-            :src="img.sizes.large" 
-            :alt="img.alt"
-            data-aos="fade"
-            data-aos-offset="200"
-      >
+      <div class="gallery">
+        <img  v-for="img in proj.acf.gallery"
+              :key="img.id"
+              :src="img.sizes.large" 
+              :alt="img.alt"
+        >
+      </div>
     </div>
   </section>
 </template>
@@ -45,6 +45,62 @@ section {
 img {
   width: 100%;
   margin-bottom: 5vw;
+}
+
+/* Desktop */
+@media screen and (min-width: 920px) {
+  .gallery {
+    width: 80%;
+    max-width: 1200px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: auto;
+    align-items: center;
+    grid-column-gap: 60px;
+    grid-row-gap: 60px;
+    margin: auto;
+  }
+
+  img {
+    width: 90%;
+  }
+
+  /* Organic grid flow */
+  img:nth-child(odd) {
+    justify-self: start;
+    transform: translateY(200px);
+  }
+
+  img:nth-child(1) {
+    width: 70%;
+  }
+
+  img:nth-child(4n) {
+    width: 80%;
+  }
+
+  img:nth-child(3n) {
+    width: 90%;
+  }
+
+  img:nth-child(5n) {
+    width: 70%;
+    justify-self: end;
+  }
+
+  img:nth-child(6n) {
+    justify-self: end;
+  }
+
+  img:nth-child(7n) {
+    width: 70%;
+    justify-self: start;
+  }
+
+  img:nth-child(8n) {
+    width: 90%;
+    justify-self: start;
+  }
 }
 
 </style>
