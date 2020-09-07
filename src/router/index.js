9f1928666d04200fe3a1b292ee15a6c7727c58f5
Charlotte Rushen <charlotterushen@hotmail.com>
2020-09-07 17:36:02 +0100

    Created Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
-import Projects from '../components/projects/Projects.vue';
-import Project from '../components/projects/Project.vue';
+import Projects from '../views/Projects.vue';
+import Project from '../views/Project.vue';
+
+import Page from '@/views/Page.vue';
 
 Vue.use(VueRouter)
 
@@ -12,6 +14,14 @@ const routes = [
     name: 'home',
     component: Home
   },
+
+  {
+    path: '/:slug',
+    name: 'page',
+    component: Page
+  },
+
+
   {
     path: '/projects',
     name: 'projects',
---
+++ src/views/Page.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="page">
+
+  </section>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style scoped>
+.page {
+  width: 90%;
+  margin: auto;
+}
+</style>
